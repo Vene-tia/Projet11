5389f46c4723da5eaaa52413ab6d3c71e5ac2ad0
Vene-tia <vene.bougato@gmail.com>
2024-01-17 17:56:47 +0100

    commit6

--- templates_part/modale.php
+++ templates_part/modale.php
@@ -2,13 +2,10 @@
   <div id="id01" class="w3-modal">
     <div class="w3-modal-content">
       <div class="w3-container">
-        <span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-display-topright">&times;</span>
-        <p>Some text</p>
+      <span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-display-topright">&times;</span>
+        <img class="contact_header" src="<?php echo get_stylesheet_directory_uri() . '/assets/contact_header.png'; ?>" alt="Contact">
+        <p>Formulaire de contact ici</p>
       </div>
     </div>
   </div>
 </div>
-
-<!-- + dans le css
-display none
-position : fixed -->
